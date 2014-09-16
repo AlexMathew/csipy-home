cf71af9898c8481025b5f031b38db4943e4bf703
Alex Mathew <alexmathew003@gmail.com>
2014-09-16 20:35:02 +0530

    Comment out the decorator

--- routes.py
+++ routes.py
@@ -6,22 +6,22 @@ import urlparse
 app = Flask(__name__)
 
 
-def connectDB(wrapped):
-    def inner(*args, **kwargs):
-        api_token = os.environ["API_TOKEN"]
-        urlparse.uses_netloc.append("postgres")
-        url = urlparse.urlparse(os.environ["DATABASE_URL"])
-        conn = psycopg2.connect(
-            database=url.path[1:],
-            user=url.username,
-            password=url.password,
-            host=url.hostname,
-            port=url.port
-        )
-        cur = conn.cursor()
-        ret = wrapped(*args, **kwargs)
-        return ret
-    return inner
+# def connectDB(wrapped):
+#     def inner(*args, **kwargs):
+#         api_token = os.environ["API_TOKEN"]
+#         urlparse.uses_netloc.append("postgres")
+#         url = urlparse.urlparse(os.environ["DATABASE_URL"])
+#         conn = psycopg2.connect(
+#             database=url.path[1:],
+#             user=url.username,
+#             password=url.password,
+#             host=url.hostname,
+#             port=url.port
+#         )
+#         cur = conn.cursor()
+#         ret = wrapped(*args, **kwargs)
+#         return ret
+#     return inner
 
 
 @app.route('/')
