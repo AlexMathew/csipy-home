061637be1ee9145f7507bccbe5c6466ef77f4d0c
Alex Mathew <alexmathew003@gmail.com>
2014-09-18 13:21:21 +0530

    Use session to include messages in register page

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session, escape, url_for
 import psycopg2
 from functools import wraps
 import os
@@ -7,6 +7,7 @@ import requests
 
 
 app = Flask(__name__)
+app.secret_key = os.environ['SECRET_KEY']
 
 
 def connectDB(wrapped):
@@ -26,6 +27,17 @@ def connectDB(wrapped):
         conn.commit()
         cur.close()
         conn.close()
+        return ret
+    return inner
+
+
+def accessSession(wrapped):
+    @wraps(wrapped)
+    def inner(*args, **kwargs):
+        msgclass = session['msgclass']
+        text = session['text']
+        ret = wrapped(msgclass, text, *args)
+        session['msgclass'] = session['text'] = None
         return ret
     return inner
 
@@ -52,6 +64,8 @@ def mailgun_operations(name, email):
 
 @app.route('/')
 def home():
+    if 'msgclass' not in session:
+        session['msgclass'] = session['text'] = None
     return render_template('home.html')
 
 
@@ -61,7 +75,7 @@ def participants(*args):
     cur = args[0]
     cur.execute('SELECT REG, NAME FROM PARTICIPANTS WHERE REGISTERED IS true ORDER BY REG')
     parts = cur.fetchall()
-    return render_template('participants.html', participants = parts)
+    return render_template('participants.html', participants=parts)
 
 
 @app.route('/setup')
@@ -70,8 +84,10 @@ def setup():
 
 
 @app.route('/register')
-def register():
-    return render_template('register.html')
+@accessSession
+def register(*args):
+    msgclass, text = args[0], args[1]
+    return render_template('register.html', msgclass=msgclass, text=text)
 
 
 @app.route('/complete', methods=['POST'])
@@ -87,15 +103,20 @@ def complete(*args):
     cur.execute('SELECT REG FROM PARTICIPANTS WHERE REGISTERED IS true')
     registered = cur.fetchall()
     if (regno,) not in complete:
-        return render_template('failure.html', error_msg="We're sorry, but the workshop is currently \
-                                open only to members registed to CSI. Try getting links to the resources \
-                                from your friends who are attending the workshop. We'll keep you \
-                                posted if we plan an open Python workshop. Thanks for your interest !")
+        session['msgclass'] = "alert alert-danger"
+        session['text'] = "We're sorry, but the workshop is currently \
+                           open only to members registed to CSI. Try getting links to the resources \
+                           from your friends who are attending the workshop. We'll keep you \
+                           posted if we plan an open Python workshop. Thanks for your interest !"
     elif (regno,) in registered:
-        return render_template('failure.html', error_msg="You're already registered.")
+        session['msgclass'] = "alert alert-danger"
+        session['text'] = "You're already registered."
     else:
+        session['msgclass'] = "alert alert-success"
+        session['text'] = "Welcome onboard ! You have registered for the workshop. \
+                           We'll keep you posted on what to do before the workshop."
         cur.execute('UPDATE PARTICIPANTS SET REGISTERED=true WHERE REG=%s', (regno,))
         cur.execute('SELECT NAME, EMAIL FROM PARTICIPANTS WHERE REG=%s', (regno,))
         name, email = cur.fetchone()
         mailgun_operations(name, email)
-        return render_template('success.html')
+    return redirect(url_for('register'))
